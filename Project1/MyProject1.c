38b80b0ae352a55a1ce08f848b9a574e01ab0c34
khalid <elhaf.khalid@gmail.com>
2025-09-05 22:24:34 +0100

    Myproject Complet

--- Project1/MyProject1.c
+++ Project1/MyProject1.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-float TNotes[100];
+#include <stdbool.h>
+float TNotes[30];
 void StarProject();
 void StarStatistique();
 int SizeGlobal;
-void AfficherNotes();
 int GetSizeTable()
 {
     int Size;
-    printf("Saisire Nombre des Notes? : ");
+    printf("Saisire Size du Tableau(Notes)? : ");
     scanf("%d", &Size);
     return Size;
+}
+void CheckNotes(int Note)
+{
+    if(Note < 0 || Note > 20)
+      printf("Error !! il Faut ");
 }
 int LireChoix()
 {
     int n;
-    printf("Entrer votre Choix? : ");
-    scanf("%d", &n);
+        printf("Entrer votre Choix? : ");
+        scanf("%d", &n);
+        if(n==7)
+        {
+            return n;
+        }
     return n;
 }
 void PauseSystem()
@@ -31,7 +40,7 @@ void AfficherMoyenne()
     {
         S += TNotes[i - 1];
     }
-    printf("La Moyenne est : %.2f", S / SizeGlobal);
+    printf("\nLa Moyenne est : %.2f\n", S / SizeGlobal);
 }
 void AfficherMax()
 {
@@ -41,7 +50,7 @@ void AfficherMax()
         if (TNotes[i] > Max)
             Max = TNotes[i];
     }
-    printf("\nLa Note Maximal est : %.2f", Max);
+    printf("\nLa Note Maximal est : %.2f\n", Max);
 }
 void AfficherMin()
 {
@@ -49,9 +58,9 @@ void AfficherMin()
     for (int i = 1;i <= SizeGlobal;i++)
     {
         if (TNotes[i - 1] < Min)
-            Min = TNotes[i];
+            Min = TNotes[i-1];
     }
-    printf("\nLa Note Minimale est : %.2f", Min);
+    printf("\nLa Note Minimale est : %.2f\n", Min);
 }
 void AfficherAdmis()
 {
@@ -61,13 +70,18 @@ void AfficherAdmis()
         if (TNotes[i - 1] >= 10)
             Count++;
     }
-    printf("\nLe Nombres eleves Admis sont : %d ", Count);
+    printf("\nLe Nombres eleves Admis sont : %d \n", Count);
 }
 void AjouteNote()
 {
     SizeGlobal++;
-    printf("\Ajoute une autre Note? : ");
-    scanf("%f", &TNotes[SizeGlobal - 1]);
+    do {
+        printf("\Ajoute une autre Note entre 0 et 20? : ");
+        scanf("%f", &TNotes[SizeGlobal - 1]);
+        CheckNotes(TNotes[SizeGlobal - 1]);
+    } while(TNotes[SizeGlobal-1] < 0 || TNotes[SizeGlobal-1]>20);
+
+    printf("\nLa Note a ete ajouter avec succeés\n");
 }
 int LireIndiceModifier()
 {
@@ -81,33 +95,49 @@ int LireIndiceModifier()
 }
 int LireIndiceSuprimer()
 {
+    SizeGlobal--;
     int indice = 0;
     do {
         printf("\nEntrer L'indice a suprimer? : ");
         scanf("%d", &indice);
-    } while (Indice < 0 || Indice > SizeGlobal);
+    } while (indice < 0 || indice > SizeGlobal);
 
-    return Indice;
+    return indice;
+}
+void AfficherNotes()
+ {
+
+    for (int i = 1;i <= SizeGlobal;i++)
+    {
+        printf("\nNotes[%d] = %.2f", i - 1, TNotes[i - 1]);
+    }
+    printf("\n");
 }
 void ModifierNote()
 {
-    printf("\nLes Notes Avant Modification sont : ");
+    printf("\nLes Notes Avant Modification sont : \n");
     AfficherNotes();
     int indice = LireIndiceModifier();
     do {
-        printf("\nModifier La Note T[%d] = ", indice);
+        printf("Modifier Notes[%d] entre 0 et 20? = ", indice);
         scanf("%f", &TNotes[indice]);
+        CheckNotes(TNotes[indice]);
     } while (TNotes[indice] < 0 || TNotes[indice]>20);
-
-    printf("\nLes Notes Apres Modification sont : ");
+    printf("\nLa note a ete Modifier avec Succee\n");
+    printf("\nLes Notes Apres Modification sont : \n");
     AfficherNotes();
 }
 void SuprimerNote()
 {
-    Printf("\nLes Notes Avant Suprimer Sont : ");
+    printf("\nLe contenue du Tableau est : \n" );
     AfficherNotes();
     int indice = LireIndiceSuprimer();
-
+    for(int i=indice;i<SizeGlobal;i++)
+    {
+        TNotes[i] = TNotes[i+1];
+    }
+    printf("\nLe contenue du Tableau Apres Suprimer Note[%d] est : ",indice);
+    AfficherNotes();
 }
 void FillTableau()
 {
@@ -115,39 +145,42 @@ void FillTableau()
     for (int i = 1;i <= Size;i++)
     {
         do {
-            printf("Entrer Note %d Entre 0 et 20 : ", i);
+            printf("Entrer Note(%d) Entre 0 et 20 : ", i);
             scanf("%f", &TNotes[i - 1]);
+            if(TNotes[i-1]<0 || TNotes[i-1]>20)
+                printf("Error !! il Faut ");
         } while (TNotes[i - 1] < 0 || TNotes[i - 1]>20);
     }
 
+    printf("\nLe Tableau a ete Remplie avec succes -)\n");
     SizeGlobal = Size;
 }
 void MenuStatistique()
 {
     system("cls");
-    printf("\n\n1-Afficher Moyenne");
+    printf("\n\tStatistique Menu");
+    printf("\n******************************");
+    printf("\n1-Afficher Moyenne");
     printf("\n2-Afficher Max");
     printf("\n3-Afficher Min");
     printf("\n4-Afficher Admis/Non Admis");
-    printf("\n5-MainMenu\n\n");
-}
-void AfficherNotes()
-{
-    for (int i = 1;i <= SizeGlobal;i++)
-    {
-        printf("\nNotes[%d] = %.2f", i - 1, TNotes[i - 1]);
-    }
+    printf("\n5-MainMenu\n");
+    printf("*******************************\n");
 }
 void MainMenu()
 {
     system("cls");
+    printf("\n\t\t\tMain Menu");
+    printf("\n****************************************************");
     printf("\n1)Saisir N notes");
     printf("\n2)Afficher les notes");
     printf("\n3)Statistiques (moyenne, max, min, admis/non admis)");
     printf("\n4)Ajouter une note");
     printf("\n5)Modifier une note (par index)");
     printf("\n6)Supprimer une note (par index)");
-    printf("\n7)Quitter\n\n");
+    printf("\n7)Quitter");
+    printf("\n****************************************************\n");
+
 }
 void PerformanceMenu(int choix)
 {
