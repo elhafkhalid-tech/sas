e3f97fce7f598ebda5c3a6811bd68b3ebdb0563d
elhafkhalid-tech <elhaf.khalid@gmail.com>
2025-09-08 18:55:08 +0200

    Modifier project2

--- Project2/project2.c
+++ Project2/project2.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 void StarProject();
-
 int NbrEtudiants = 0;
-struct stEtudiant
+ struct stEtudiant
 {
     char  CNE[20];
     char  nom[40];
@@ -12,13 +11,14 @@ struct stEtudiant
     float notes[4];
     float moyenne;
 };
-struct stEtudiant Classe[30];
+
+struct stEtudiant Classe[100];
+
 int LirePosition()
 {
     int Position;
     printf("\nEntrer Position d'etudiant : ");
     scanf("%d",&Position);
-
     return Position;
 }
 bool CheckPosition(int Position)
@@ -27,7 +27,7 @@ bool CheckPosition(int Position)
     {
         printf("\nPosition Invalid!!\n");
     }
-    return true;
+    
 }
 int LireChoix()
 {
@@ -43,14 +43,28 @@ void SystemPause()
     printf("\n");
     system("pause");
 }
+float MoyenneEtudiant(int Position)
+{
+    Position = LirePosition();
+    float Somme = 0;
+    for(int i=0;i<4;i++)
+    {
+        Somme += Classe[Position].notes[i];
+    }
+    return Somme/4;
+}
+void GetMoyenneGenerale()
+{
+   
+}
 void MainMenu()
 {
     system("cls");
-    printf("\n1) Ajouter un étudiant");
-    printf("\n2) Saisir les notes un étudiant");
-    printf("\n3) Afficher tous les étudiants");
-    printf("\n4) Afficher bulletin d’un étudiant");
-    printf("\n5) Calculer la moyenne générale");
+    printf("\n1) Ajouter un etudiant");
+    printf("\n2) Saisir les notes un etudiant");
+    printf("\n3) Afficher tous les etudiants");
+    printf("\n4) Afficher bulletin d'un etudiant");
+    printf("\n5) Calculer la moyenne generale");
     printf("\n6) Quitter\n");
 }
 void Initialisation_DesNotes_NbrEtudiants_Moyenne()
@@ -75,41 +89,48 @@ void SaisirNotes()
         printf("\nInvalid position\n");
         return;
     }
-
     for(int i=1;i<=4;i++)
     {
-        printf("Entrer Note(%d) : ",i);
+        printf("\n\nEntrer Note(%d) : ",i);
         scanf("%f",&Classe[Position].notes[i-1]);
     }
 }
 void AjouterEtudiant()
 {
-   if(NbrEtudiants>=30)
+   if(NbrEtudiants>=100)
    {
        printf("\nClass Plein, impossible ajouter plus etudiant");
        return;
    }
-   printf("\nEntrer Le Nom? : ");
-   scanf("%s",Classe[NbrEtudiants].nom);
-   printf("Entrer Le PreNom? : ");
-   scanf("%s",Classe[NbrEtudiants].prenom);
-   printf("Entrer Le CNE? : ");
-   scanf("%s",Classe[NbrEtudiants].CNE);
-   printf("\nEtudiant AJoute avec succes\n");
-
-   Initialisation_DesNotes_NbrEtudiants_Moyenne();
-
+      printf("\nEntrer Le Nom? : ");
+      scanf("%s",Classe[NbrEtudiants].nom);
+      printf("Entrer Le PreNom? : ");
+      scanf("%s",Classe[NbrEtudiants].prenom);
+      printf("Entrer Le CNE? : ");
+      scanf("%s",Classe[NbrEtudiants].CNE);
+      printf("\nEtudiant AJoute avec succes\n");
+   
+     Initialisation_DesNotes_NbrEtudiants_Moyenne();
+}
+void AfficherBulletinDunEtudiant()
+{
+        int position = LirePosition();
+        //CheckPosition(position);
+        printf("\nEtudiant (%d) : ", position+1);
+        printf("\nPreNom est : %s ",Classe[position].prenom);
+        printf("\nNom est : %s ",Classe[position].nom);
+        printf("\nCNE est : %s ",Classe[position].CNE);
+        printf("\nLes Notes Sont : [%.2f , %.2f , %.2f , %.2f] \n\n", Classe[position].notes[0], Classe[position].notes[1],
+                                        Classe[position].notes[2], Classe[position].notes[3]);
 }
 void AfficherEtudiants()
 {
-    for(int i=1;i<=NbrEtudiants;i++)
+     for(int i=1;i<=NbrEtudiants;i++)
     {
-        printf("\nEtudiant (%d) : ", i);
+        printf("\n\nEtudiant (%d) : ", i);
         printf("\nPreNom est : %s ",Classe[i-1].prenom);
         printf("\nNom est : %s ",Classe[i-1].nom);
         printf("\nCNE est : %s ",Classe[i-1].CNE);
-        printf("\nLes Notes Sont : [%.2f , %.2f , %.2f , %.2f] \n\n", Classe[i-1].notes[0], Classe[i-1].notes[1],
-                                        Classe[i-1].notes[2], Classe[i-1].notes[3]);
     }
 }
 void GestionMenu(int Choix)
@@ -120,7 +141,6 @@ void GestionMenu(int Choix)
         AjouterEtudiant();
         SystemPause();
         StarProject();
-
         break;
     case 2:
         SaisirNotes();
@@ -133,7 +153,7 @@ void GestionMenu(int Choix)
         StarProject();
         break;
     case 4:
-        //AfficherBulletin();
+        AfficherBulletinDunEtudiant();
         SystemPause();
         StarProject();
         break;
